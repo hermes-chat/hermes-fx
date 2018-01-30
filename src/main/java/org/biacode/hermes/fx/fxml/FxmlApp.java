2950792f9a4c0d69d69327ad7811cdcf3c55b3f7
Arthur Asatryan <biacoder@gmail.com>
2018-01-30 23:59:26 +0400

    Apply #22 changes.

--- src/main/java/org/biacode/hermes/fx/fxml/FxmlApp.java
+++ src/main/java/org/biacode/hermes/fx/fxml/FxmlApp.java
@@ -1,5 +1,6 @@
 package org.biacode.hermes.fx.fxml;
 
+import io.netty.channel.Channel;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -8,25 +9,39 @@ import javafx.stage.Stage;
 
 import java.util.Objects;
 
+import static org.biacode.hermes.fx.fxml.network.WebSocketClient.getClientChannel;
+
 /**
  * Created by Arthur Asatryan.
  * Date: 1/30/18
  * Time: 12:41 AM
  */
 public class FxmlApp extends Application {
+
+    public static Channel CHANNEL;
+
     @Override
     public void start(final Stage primaryStage) throws Exception {
         final Parent root = FXMLLoader.load(Objects.requireNonNull(
                 getClass()
                         .getClassLoader()
-                        .getResource("sample.fxml")
+                        .getResource("mainView.fxml")
         ));
         primaryStage.setTitle("HermesChat");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
+        primaryStage.setOnCloseRequest(event -> {
+            CHANNEL.close();
+        });
     }
 
     public static void main(String[] args) {
+        try {
+            CHANNEL = getClientChannel()
+                    .orElseThrow(() -> new IllegalArgumentException("can not connect to the websocket server"));
+        } catch (final Exception e) {
+            e.printStackTrace();
+        }
         launch(args);
     }
 }
